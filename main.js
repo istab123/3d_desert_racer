5a9bc0882ccdc8341ac89e2eac28fa489a1ccced
istab123 <Sebastian.Burger1@outlook.de>
2025-08-12 13:17:15 +0200

    Fix Sky import so game renders correctly

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'https://unpkg.com/three@0.157.0/build/three.module.js';
-import { Sky } from 'https://unpkg.com/three@0.157.0/examples/jsm/objects/Sky.js';
+import { Sky } from 'https://unpkg.com/three@0.157.0/examples/jsm/objects/Sky.js?module';
 
 // --- Renderer & Scene ---
 const canvas = document.getElementById('game');
